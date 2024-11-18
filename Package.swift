92d3e61674ed8f2111f80cd1377247e333933024
trinhxuanminh2000 <trinhxuanminh2000@gmail.com>
2024-11-18 11:20:32 +0700

    fix package

--- Package.swift
+++ Package.swift
@@ -12,15 +12,11 @@ let package = Package(
       name: "IAPManager",
       targets: ["IAPManager"]),
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-  ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "IAPManager",
-      dependencies: [
-      ])
+      name: "IAPManager"
+    )
   ]
 )
